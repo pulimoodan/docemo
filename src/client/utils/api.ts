be62e15bda5ba31e9a4b96d8f67ca1032b114f77
pulimoodan <akbaraliongo@gmail.com>
2022-09-11 14:05:23 +0530

    resolves cors 2

--- src/client/utils/api.ts
+++ src/client/utils/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3000/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
